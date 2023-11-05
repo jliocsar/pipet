6850a0591bb6454fc1dade0f4e534ec41a006b43
jliocsar <juliocesarmfo@gmail.com>
2023-11-05 18:51:39 -0300

    update docs

--- .config/typedoc.js
+++ .config/typedoc.js
@@ -1,9 +1,14 @@
 module.exports = {
   $schema: 'https://typedoc.org/schema.json',
+  name: 'pipet',
   entryPoints: ['../pipet.ts'],
   out: '../docs',
   readme: '../readme.docs.md',
   darkHighlightTheme: 'rose-pine',
+  navigationLinks: {
+    Repository: 'https://github.com/jliocsar/pipet',
+  },
+  gitRemote: 'origin',
   excludeInternal: true,
   excludePrivate: true,
   excludeProtected: true,
